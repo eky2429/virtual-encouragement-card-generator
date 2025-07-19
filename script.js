6bb075f8f645df1ce0a05f71b4d77d8a0c217e24
Ethan Ky <eky2429@gmail.com>
2025-07-19 17:08:26 -0500

    Added some styling / background selecing

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function onSubmit() {
-    var name1 = document.getElementById("name1").value;
-    var body_text = document.getElementById("body").value;
-    var name2 = document.getElementById("name2").value;
+    const name1 = document.getElementById("name1").value;
+    const body_text = document.getElementById("body").value;
+    const name2 = document.getElementById("name2").value;
+    const bg = document.getElementById("bg-select").value;
 
-    var result = document.getElementById("result");
-    
+    const result = document.getElementById("result");
+
+    // Reset previous background class
+    result.className = 'card'; // always start with "card" class
+
+    if (bg !== 'none') {
+        result.classList.add(bg);
+    }
+
     result.innerHTML = `Dear ${name1},<br><br>${body_text}<br><br>From, ${name2}`;
 }
