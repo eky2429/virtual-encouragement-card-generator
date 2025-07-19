330b7d76ea51b2679464413c398e5f7cfe65dd21
Ethan Ky <eky2429@gmail.com>
2025-07-19 17:20:28 -0500

    Added buttons to download / copy card

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function onSubmit() {
         result.classList.remove('flip'); // Remove it after a second so you can reuse
     }, 1000);
 }
+
+function downloadCard() {
+  const element = document.createElement("a");
+  const cardContent = document.getElementById("result").innerHTML;
+  const file = new Blob([cardContent.replace(/<br>/g, "\n")], { type: 'text/plain' });
+  element.href = URL.createObjectURL(file);
+  element.download = "encouragement_card.txt";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+function copyCard() {
+  const temp = document.createElement("textarea");
+  temp.value = document.getElementById("result").innerText;
+  document.body.appendChild(temp);
+  temp.select();
+  document.execCommand("copy");
+  document.body.removeChild(temp);
+  alert("Card copied to clipboard!");
+}
